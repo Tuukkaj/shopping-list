25ca7131fe77d9f1a7e97877e5cc9952dec27640
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-21 11:00:13 +0200

    ShoppingListMain: Create JavaFX window.

--- src/main/java/shoppingList/ShoppingListMain.java
+++ src/main/java/shoppingList/ShoppingListMain.java
@@ -1,7 +1,26 @@
 package shoppingList;
 
-public class ShoppingListMain {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ShoppingListMain extends Application {
+
     public static void main(String... args) {
         System.out.println("Author: Tuukka Juusela <tuukka.juusela@cs.tamk.fi>");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage stage) {
+        Scene content = new Scene(new StackPane(), 640,420);
+        stage.setScene(content);
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.centerOnScreen();
+        stage.setTitle("Shopping List");
+        stage.show();
+    }
+
 }
