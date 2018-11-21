4db6264bd28d3bbf73ab10b3bad806d00825ba56
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-21 10:50:46 +0200

    Initial commit

---
+++ src/main/java/shoppingList/ShoppingListMain.java
@@ -0,0 +1,7 @@
+package shoppingList;
+
+public class ShoppingListMain {
+    public static void main(String... args) {
+        System.out.println("Author: Tuukka Juusela <tuukka.juusela@cs.tamk.fi>");
+    }
+}
