7d84d5f259dc9af9525ab3885e26860efb2ceab0
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-02 22:23:47 +0200

    ShoppingListMain: Add Javadoc.

--- src/main/java/shoppingList/ShoppingListMain.java
+++ src/main/java/shoppingList/ShoppingListMain.java
@@ -6,13 +6,28 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+/**
+ * Main class of the program. Holds methods for reading and writing JSON files. Also holds methods for creation of
+ * JSONComponents that are part of the JSONFileData.
+ *
+ * @author Tuukka Juusela
+ * @version 2018.0212
+ * @since 1.8
+ */
 public class ShoppingListMain extends Application {
-
+    /**
+     * Main method of the program. Prints authors name and launches program.
+     * @param args command line argument. Not in use.
+     */
     public static void main(String... args) {
         System.out.println("Author: Tuukka Juusela <tuukka.juusela@cs.tamk.fi>");
         launch(args);
     }
 
+    /**
+     * Starts the JavaFX. Creates new Components and components create borderPane for Scene.
+     * @param stage to show.
+     */
     @Override
     public void start(Stage stage) {
         Components components = new Components(stage, this);
