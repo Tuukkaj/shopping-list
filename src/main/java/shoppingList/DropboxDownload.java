7250d09ac103bc52fc1c8a378a8853a5343d48c2
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-05 10:23:05 +0200

    DropboxDownload: Add generateTable().
    generateTable(): Used to create TableView of Files in users dropbox.

--- src/main/java/shoppingList/DropboxDownload.java
+++ src/main/java/shoppingList/DropboxDownload.java
@@ -1,8 +1,13 @@
 package shoppingList;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class DropboxDownload {
@@ -16,6 +21,18 @@ public class DropboxDownload {
         ((Stage) dialog.getDialogPane().getScene().getWindow()).getIcons()
                 .add(new Image(getClass().getResourceAsStream("icons/dropbox.png")));
 
+        dialog.getDialogPane().setContent(generateTable());
 
+    }
+
+    private HBox generateTable() {
+        HBox tableBox = new HBox();
+        TableView<String> tableView = new TableView<>();
+        ObservableList<String> oList = FXCollections.observableArrayList();
+        tableView.setItems(oList);
+        TableColumn<String, String> fileColumn = new TableColumn<>("File");
+        tableBox.getChildren().addAll(tableView);
+
+        return tableBox;
     }
 }
