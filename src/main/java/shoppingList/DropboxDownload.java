7917f23fec1aa7ea9aa688773bfe60a66f4b497a
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-05 10:07:58 +0200

    DropboxDownload: Add generateFilePicker().

--- src/main/java/shoppingList/DropboxDownload.java
+++ src/main/java/shoppingList/DropboxDownload.java
@@ -1,4 +1,16 @@
 package shoppingList;
 
+import javafx.scene.control.Dialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 public class DropboxDownload {
+
+    private void generateFilePicker() {
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/dropbox.png"))));
+        ((Stage) dialog.getDialogPane().getScene().getWindow()).getIcons()
+                .add(new Image(getClass().getResourceAsStream("icons/dropbox.png")));
+    }
 }
