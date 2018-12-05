bc6c184a71c4db2342335de8bc78cb9a160b4574
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-05 10:48:53 +0200

    DropboxDownload: Add class FileItem. Update generateTable().

--- src/main/java/shoppingList/DropboxDownload.java
+++ src/main/java/shoppingList/DropboxDownload.java
@@ -2,12 +2,16 @@ package shoppingList;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DropboxDownload {
@@ -20,20 +24,44 @@ public class DropboxDownload {
         dialog.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/dropbox.png"))));
         ((Stage) dialog.getDialogPane().getScene().getWindow()).getIcons()
                 .add(new Image(getClass().getResourceAsStream("icons/dropbox.png")));
-
-        dialog.getDialogPane().setContent(generateTable());
+        dialog.setTitle("Choose file to download");
+        VBox tableBox = generateTable();
+        dialog.getDialogPane().setContent(tableBox);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
         dialog.showAndWait();
-
     }
 
-    private HBox generateTable() {
-        HBox tableBox = new HBox();
-        TableView<String> tableView = new TableView<>();
-        ObservableList<String> oList = FXCollections.observableArrayList();
-        tableView.setItems(oList);
-        TableColumn<String, String> fileColumn = new TableColumn<>("File");
-        tableBox.getChildren().addAll(tableView);
+    private VBox generateTable() {
+        VBox tableBox = new VBox();
 
+        TableView<FileItem> tableView = new TableView<>();
+
+        ObservableList<FileItem> oList = FXCollections.observableArrayList();
+        oList.addAll(new FileItem("example.json"));
+
+        TableColumn<FileItem, String> fileColumn = new TableColumn<>("File");
+        fileColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        fileColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        fileColumn.setPrefWidth(200);
+        tableView.getColumns().addAll(fileColumn);
+        tableView.setItems(oList);
+        tableBox.getChildren().addAll(tableView);
         return tableBox;
+    }
+
+    public class FileItem {
+        String name;
+
+        FileItem(String name) {
+            setName(name);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
