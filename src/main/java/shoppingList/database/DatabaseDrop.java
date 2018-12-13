1cccc3dce7ee02c3e061374a007c23953b275797
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-13 10:38:46 +0200

    DatabaseDrop: Add.

---
+++ src/main/java/shoppingList/database/DatabaseDrop.java
@@ -0,0 +1,14 @@
+package shoppingList.database;
+
+public class DatabaseDrop {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/TuukkaLister;";
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public void drop() {
+
+    }
+
+
+}
