211bd6a538a6129826a61aeeeba76698089b392e
Tuukkaj <juusela.tuukka@gmail.com>
2018-12-13 11:03:23 +0200

    DatabaseErrorDialogs: Add generateError().

--- src/main/java/shoppingList/database/DatabaseErrorDialogs.java
+++ src/main/java/shoppingList/database/DatabaseErrorDialogs.java
@@ -1,4 +1,18 @@
 package shoppingList.database;
 
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 public class DatabaseErrorDialogs {
+    private void generateError() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("shoppingList/icons/h2.png")));
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText("Something went horribly wrong.\nPlease contact the developer");
+        alert.showAndWait();
+    }
 }
